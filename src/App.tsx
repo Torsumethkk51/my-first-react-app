6ed1cd53a13a72a86a52bb666f9aff9c79a4603e
Torsumethkk51 <torsumethkkhoyo@gmail.com>
2025-08-02 00:02:29 +0700

    Add source code

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="w-full min-h-screen h-auto py-8">
+      <h1 className="text-3xl text-center font-bold text-white">Hello React App!</h1>
+    </div>
+  )
+}
